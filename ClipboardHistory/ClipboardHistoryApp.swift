aa4e5fbce46e2ca3d3d3d24ba0eff67de1a56e56
IBeckzzzz <thiagobeck35@icloud.com>
2024-06-20 14:45:39 -0300

    Initial Commit

---
+++ ClipboardHistory/ClipboardHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardHistoryApp.swift
+//  ClipboardHistory
+//
+//  Created by Thiago Beck De Andrade.T_STEFANINI on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardHistory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipboardHistory
+//
+//  Created by Thiago Beck De Andrade.T_STEFANINI on 20/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
